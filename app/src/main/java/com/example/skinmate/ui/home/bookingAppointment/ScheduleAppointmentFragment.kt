afa055d12b855aa745765c83d016d187cf863c60
Gautham2455 <gautham.prabhu@digital.datamatics.com>
2021-05-11 16:34:23 +0530

    time slots added from api

--- app/src/main/java/com/example/skinmate/ui/home/bookingAppointment/ScheduleAppointmentFragment.kt
+++ app/src/main/java/com/example/skinmate/ui/home/bookingAppointment/ScheduleAppointmentFragment.kt
@@ -14,6 +14,8 @@ import android.widget.Button
 import android.widget.CalendarView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.skinmate.BaseFragment
 import com.example.skinmate.R
 import com.example.skinmate.ui.auth.SignInFragment
@@ -22,6 +24,10 @@ import com.example.skinmate.ui.home.HomeViewModel
 class ScheduleAppointmentFragment :BaseFragment(){
 
     private val viewModel by viewModels<HomeViewModel>()
+    var MorningSlots= mutableListOf<String>("09:00","09:10","09:20","09:30","09:40","09:50","10:00","10:10","10:20","10:30","10:40","10:50","11:00","11:10","11:20","11:30",
+        "11:40","11:50")
+    var AfternoonSlots= mutableListOf<String>("01:00","01:10","01:20","01:30","01:40","01:50","02:00","02:10","02:20","02:30","02:40","02:50","03:00","03:10","03:20","03:30",
+        "03:40","03:50","04:00","04:10","04:20","04:30","04:40","04:50","05:00")
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
@@ -38,18 +44,35 @@ class ScheduleAppointmentFragment :BaseFragment(){
         val sharedPref: SharedPreferences =requireActivity()!!.getSharedPreferences("SkinMate", Context.MODE_PRIVATE)
         val token="Bearer "+sharedPref!!.getString(SignInFragment.TOKEN,"none")
         viewModel.getBookedAppointments(token,SlectDoctorFragment.doctorID!!,"2021-05/13").observe(requireActivity()){
-            var ActualAvalibaleSlots:Array<String>?=null
+
             Log.v("Book",it[0].responseInformation.toString())
-            if(it[0].code==200){
-                if(it[0].responseInformation.isEmpty()){
 
+            val rv_morningSlots=view.findViewById<RecyclerView>(R.id.morning_slots)
+            val rv_afternoonSlots=view.findViewById<RecyclerView>(R.id.afternoon_slots)
+
+            if(it[0].code==200) {
+                if (it[0].responseInformation.isEmpty()) {
+
+                } else {
+
+                    for (bookedSlots in it[0].responseInformation) {
+                        if(MorningSlots.contains(bookedSlots)){
+                            MorningSlots.remove(bookedSlots)
+                        }
+                        if(AfternoonSlots.contains(bookedSlots))
+                            AfternoonSlots.remove(bookedSlots)
+                    }
                 }
-                    //for(availableSlots in resources.getStringArray(R.array.morningSlots))
-
-            }else
-            {
+            }else   {
 
             }
+            val morningSlots_adapter=TimeSlotAdapter(MorningSlots)
+            rv_morningSlots.layoutManager = GridLayoutManager(context,3)
+            val afternoonSlots_adapter=TimeSlotAdapter(AfternoonSlots)
+            rv_morningSlots.setAdapter(morningSlots_adapter)
+            rv_afternoonSlots.layoutManager = GridLayoutManager(context,3)
+            rv_afternoonSlots.setAdapter(afternoonSlots_adapter)
+            Log.v("Slots",AfternoonSlots.toString())
         }
 
 
@@ -66,10 +89,14 @@ class ScheduleAppointmentFragment :BaseFragment(){
         return view
     }
 
-    fun append(arr: Array<Int>, element: Int): Array<Int> {
-        val list: MutableList<Int> = arr.toMutableList()
-        list.add(element)
-        return list.toTypedArray()
+    fun remove(arr: Array<String>, index: Int): Array<String> {
+        if (index < 0 || index >= arr.size) {
+            return arr
+        }
+
+        val result = arr.toMutableList()
+        result.removeAt(index)
+        return result.toTypedArray()
     }
 
     companion object{
