9197e608b5c76459438de7462454f5e9064f911f
Gautham2455 <gautham.prabhu@digital.datamatics.com>
2021-05-11 10:55:04 +0530

    Sub service and doctor list added

--- app/src/main/java/com/example/skinmate/data/responses/ResponseInformation.kt
+++ app/src/main/java/com/example/skinmate/data/responses/ResponseInformation.kt
@@ -4,8 +4,10 @@ package com.example.skinmate.data.responses
 import com.google.gson.annotations.SerializedName
 
 data class ResponseInformation(
-    //@SerializedName("1")
+    @SerializedName("1")
     val x1: String,
-    //@SerializedName("2")
-    val x2: String
+    @SerializedName("2")
+    val x2: String,
+    @SerializedName("3")
+    val x3: String
 )
