c124580e8c7ae9954620f40ecadea406c7195ef5
Amulya-MB <amulya.b@digital.datamatics.com>
2021-05-18 10:14:56 +0530

    layout changes

--- app/src/main/java/com/example/skinmate/ui/home/MessageService.kt
+++ app/src/main/java/com/example/skinmate/ui/home/MessageService.kt
@@ -1,12 +1,10 @@
 package com.example.skinmate.ui.home
 
 import android.util.Log
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MessageService : FirebaseMessagingService() {
-
 
     override fun onNewToken(s: String) {
         Log.d("Token", "Refreshed token: $s")
--- app/src/main/java/com/example/skinmate/ui/home/appointments/AppointmentListFragment.kt
+++ app/src/main/java/com/example/skinmate/ui/home/appointments/AppointmentListFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.skinmate.BaseFragment
